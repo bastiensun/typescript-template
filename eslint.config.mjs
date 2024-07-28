c5b0e35d7aa4644cf4a0e7f6b60a9068c5a4fb99
Bastien Sun <43788700+bastiensun@users.noreply.github.com>
2024-07-28 14:27:32 +0200

    chore: lint with type information

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,5 +4,18 @@ import ts from "typescript-eslint";
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   js.configs.recommended,
-  ...ts.configs.recommended,
+  ...ts.configs.strictTypeChecked,
+  ...ts.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    ...ts.configs.disableTypeChecked,
+  },
 ];
