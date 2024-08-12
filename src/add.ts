b69eeab193d4c1c0c1728913ad1673d98232617d
Bastien Sun <43788700+bastiensun@users.noreply.github.com>
2024-08-12 14:11:34 +0200

    chore: add eslint rules

--- src/add.ts
+++ src/add.ts
@@ -1,14 +1,17 @@
 export function add(...arguments_: number[]): number {
-  return arguments_.reduce((a, b) => a + b, 0);
+  const initialValue = 0;
+  return arguments_.reduce((a, b) => a + b, initialValue);
 }
 
 // Stryker disable all
 if (import.meta.vitest) {
-  const { expect, it } = import.meta.vitest;
+  const { expect, test } = import.meta.vitest;
 
-  it("add", () => {
-    expect(add()).toBe(0);
-    expect(add(1)).toBe(1);
-    expect(add(1, 2, 3)).toBe(6);
+  test("add", () => {
+    /* eslint-disable @typescript-eslint/no-magic-numbers */
+    expect(add()).toStrictEqual(0);
+    expect(add(1)).toStrictEqual(1);
+    expect(add(1, 2, 3)).toStrictEqual(6);
+    /* eslint-enable @typescript-eslint/no-magic-numbers */
   });
 }
