4d45397a1b7d7c038faf23852e6108949c52cf1d
Bastien Sun <43788700+bastiensun@users.noreply.github.com>
2024-08-21 10:52:52 +0200

    style: remove semicolons at the ends of statements
    cf. https://www.epicweb.dev/your-code-style-does-matter-actually#the-case-against-semicolons

--- src/add.ts
+++ src/add.ts
@@ -1,17 +1,17 @@
 export function add(...arguments_: number[]): number {
-  const initialValue = 0;
-  return arguments_.reduce((a, b) => a + b, initialValue);
+  const initialValue = 0
+  return arguments_.reduce((a, b) => a + b, initialValue)
 }
 
 // Stryker disable all
 if (import.meta.vitest) {
-  const { expect, test } = import.meta.vitest;
+  const { expect, test } = import.meta.vitest
 
   test("add", () => {
     /* eslint-disable @typescript-eslint/no-magic-numbers */
-    expect(add()).toStrictEqual(0);
-    expect(add(1)).toStrictEqual(1);
-    expect(add(1, 2, 3)).toStrictEqual(6);
+    expect(add()).toStrictEqual(0)
+    expect(add(1)).toStrictEqual(1)
+    expect(add(1, 2, 3)).toStrictEqual(6)
     /* eslint-enable @typescript-eslint/no-magic-numbers */
-  });
+  })
 }
